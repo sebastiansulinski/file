99f8169eae86b10679fe34c9470b95b5464ec84f
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2014-11-27 20:43:54 +0000

    Initial commit

---
+++ tests/FileTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use \SSD\File\File;
+
+
+class FileTest extends \PHPUnit_Framework_TestCase {
+
+
+    protected $file;
+
+
+    protected function setUp()
+    {
+
+        $this->file = '';
+
+    }
+
+
+
+    /**
+     * @expectedException \SSD\File\Exception\InvalidArgument
+     */
+    public function testConstructor()
+    {
+
+        $file = new File('/some/file');
+
+    }
+
+
+}
