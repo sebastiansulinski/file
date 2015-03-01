a2c9e45a5ce32a5f022900ba8995a6e75903efcd
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-03-01 09:50:55 +0000

    Added annotation

--- src/File/Mime/Extractor/ExtensionExtractor.php
+++ src/File/Mime/Extractor/ExtensionExtractor.php
@@ -1,19 +1,26 @@
-<?php
-
-namespace SSD\File\Mime\Extractor;
+<?php namespace SSD\File\Mime\Extractor;
 
 
-use \SSD\File\File;
-use \SSD\File\Mime\MimeMap;
+use SSD\File\File;
+use SSD\File\Mime\MimeMap;
 
 
 class ExtensionExtractor implements ExtractorInterface {
 
 
+    /**
+     * Instance of the File object.
+     *
+     * @var File
+     */
     private $file;
 
 
-
+    /**
+     * Constructor.
+     *
+     * @param File $file
+     */
     public function __construct(File $file)
     {
 
@@ -22,6 +29,13 @@ class ExtensionExtractor implements ExtractorInterface {
     }
 
 
+    /**
+     * Return MimeMap instance.
+     *
+     * @throws \SSD\File\Exception\NonExistentIndex
+     *
+     * @return string
+     */
     public function type() {
 
         return MimeMap::byExtension($this->file->extension());
